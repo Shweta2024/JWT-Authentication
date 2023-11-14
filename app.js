a05dcbcc992c1622d7326dd3841f87f45f0ff079
Shweta Bhagat <bhagatshweta0216@gmail.com>
2023-11-14 21:43:15 +0530

    create private route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const bodyParser = require('body-parser')
 const route = require('./routes/auth')
+const privateRoute = require('./routes/privateRoute')
 
 const app = express()
 const PORT = process.env.PORT || 5000
@@ -12,6 +13,7 @@ app.use(bodyParser.urlencoded({extended:true}))
 mongoose.connect(process.env.DB_CONNECTION_STRING)
 
 app.use('/api/user', route)
+app.use(privateRoute)
 
 app.listen(PORT, (req, res) => {
     console.log(`server started at port: ${PORT}`)
---
+++ routes/privateRoute.js
@@ -0,0 +1,25 @@
+const router = require('express').Router()
+const jwt = require('jsonwebtoken')
+
+// middleware to verify token
+function verifyToken(req, res, next) {
+
+    if (!req.header('auth-token'))
+        return res.status(401).send('Access Denied!')
+
+    try {
+        // we get an object containg feild(that we used to create the token) and iat
+        const userID = jwt.verify(req.header('auth-token'), process.env.JWT_SECRET)
+        req.user = userID
+        next()
+    }
+    catch (error) {
+        return res.status(400).send('Invalid Token!')
+    }
+}
+
+router.get('/home', verifyToken, (req, res) => {
+    res.send('welcome')
+})
+
+module.exports = router
